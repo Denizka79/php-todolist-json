14b9887690dc6123a7b1ae07529872befedad0f3
Denis Meshcheryakov <denismeshcheryakov@MacBook-Air-Denis.local>
2023-04-11 00:06:08 +0300

    Added task list

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php if (file_exists("todo.json")) {
+    $json = file_get_contents("todo.json");
+    $todoitems = json_decode($json, true);
+} ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,10 +12,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="newtask.php" method="POST">
+    <form class="new-task" action="newtask.php" method="POST">
         <!-- <label for="newtask">Enter new task: </label> -->
         <input type="text" name="taskname" placeholder="Enter your new task">
         <button type="submit">Go</button>
     </form>
+    <table>
+        <tr>
+            <th>Task</th>
+            <th>Complete?</th>
+            <th>Action</th>
+        </tr>
+        <?php
+        foreach ($todoitems as $todoName => $todo) {
+            $complete = "";
+            if ($todo["complete"]) {
+                $complete = "checked";
+            }
+            echo "<tr><td>" . $todoName . "</td><td><input type='checkbox' " . $complete . "></td>" . "<td><form action='delete.php' method='post'><input type='hidden' name='todo_name' value='" . $todoName ."'><button>Delete</button></form></td></tr>";
+        } ?>
+    </table>
 </body>
 </html>
